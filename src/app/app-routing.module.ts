b9a1c8a928955ffb24ec4cbea3b0c849674c3ee8
jorgefg22 <jorgefernandezgonzales.22@gmail.com>
2023-11-08 16:41:37 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { UpdateComponent } from './pages/update/update.component';
+import { ReviewComponent } from './pages/review/review.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/login',pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'update',component:UpdateComponent},
+  {path:'review',component:ReviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { UpdateComponent } from './pages/update/update.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ReviewComponent } from './pages/review/review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    RegisterComponent,
+    UpdateComponent,
+    LoginComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
